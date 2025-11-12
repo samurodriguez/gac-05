14594aa98f9840e412f88dd943bc595d875e2c2e
Samuel Rodríguez <samuelrodriguezrey@outlook.com>
2025-11-12 16:49:54 +0100

    add slack webhook url input

--- .github/actions/notify-slack/main.js
+++ .github/actions/notify-slack/main.js
@@ -1,6 +1,13 @@
 const message = process.env.INPUT_MESSAGE;
+const slackWebhookUrl = process.env.INPUT_SLACK_WEBHOOK_URL;
 
 if (!message) {
+  console.error("missing 'message' required param");
+  process.exit(1);
+}
+
+if (!slackWebhookUrl) {
+  console.error("missing 'slack_webhook_url' required param");
   process.exit(1);
 }
 
